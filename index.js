c94373ae1852fe6fdec130e89eb6d83bc6565751
Calvin French-Owen <calvin@segment.io>
2013-03-04 00:09:31 -0800

    Updating prototype

--- index.js
+++ index.js
@@ -30,70 +30,70 @@ function createCollection (Model) {
   if (this instanceof createCollection) return new Collection(Model);
 
   Collection.prototype.Model = Model;
+
+  /**
+   * Mixin enumerable.
+   */
+
+  Enumerable(Collection.prototype);
+
+  /**
+   * Mixin emitter.
+   */
+
+  Emitter(Collection.prototype);
+
+  /**
+   * Iterator implementation.
+   */
+
+  Collection.prototype.__iterate__ = function(){
+    var self = this;
+    return {
+      length: function(){ return self.length() },
+      get: function(i){ return self.models[i] }
+    }
+  };
+
+  /**
+   * Return the collection length.
+   *
+   * @return {Number}
+   * @api public
+   */
+
+  Collection.prototype.length = function(){
+    return this.models.length;
+  };
+
+  /**
+   * Add `model` to the collection and return the index.
+   *
+   * @param {Object} model
+   * @return {Number}
+   * @api public
+   */
+
+  Collection.prototype.push = function(model){
+    this.models.push(model);
+    this.emit('push', model);
+    return this.models.length;
+  };
+
+  /**
+   * Remove `model` from the collection.
+   *
+   * @param {Object} model
+   * @api public
+   */
+
+  Collection.prototype.remove = function(model){
+    var index = this.models.indexOf(model);
+    if (index > -1) {
+      this.models.splice(index, 1);
+      this.emit('remove', model);
+    }
+  };
+
   return Collection;
 }
-
-
-/**
- * Mixin enumerable.
- */
-
-Enumerable(Collection.prototype);
-
-/**
- * Mixin emitter.
- */
-
-Emitter(Collection.prototype);
-
-/**
- * Iterator implementation.
- */
-
-Collection.prototype.__iterate__ = function(){
-  var self = this;
-  return {
-    length: function(){ return self.length() },
-    get: function(i){ return self.models[i] }
-  }
-};
-
-/**
- * Return the collection length.
- *
- * @return {Number}
- * @api public
- */
-
-Collection.prototype.length = function(){
-  return this.models.length;
-};
-
-/**
- * Add `model` to the collection and return the index.
- *
- * @param {Object} model
- * @return {Number}
- * @api public
- */
-
-Collection.prototype.push = function(model){
-  this.models.push(model);
-  this.emit('push', model);
-  return this.models.length;
-};
-
-/**
- * Remove `model` from the collection.
- *
- * @param {Object} model
- * @api public
- */
-
-Collection.prototype.remove = function(model){
-  var index = this.models.indexOf(model);
-  if (index > -1) {
-    this.models.splice(index, 1);
-    this.emit('remove', model);
-  }
-};
